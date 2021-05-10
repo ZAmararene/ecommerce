40970cfa779025b9c20560d1802910218a1eb3fd
AMARARENE <zina.amararene@gmail.com>
2021-05-10 14:44:29 +0200

    security implementation

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -6,15 +6,20 @@ use App\Entity\Category;
 use App\Form\CategoryType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\String\Slugger\SluggerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class CategoryController extends AbstractController
 {
     /**
      * @Route("/admin/category/create", name="category_create")
+     * @IsGranted("ROLE_ADMIN", message="Vous n'avez pas le droit d'accéder à cette ressource")
      */
     public function create(Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
     {
@@ -40,8 +45,46 @@ class CategoryController extends AbstractController
     /**
      * @Route("/admin/category/{id}/edit", name="category_edit")
      */
-    public function edit(Category $category, Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger)
+    public function edit(Category $category, Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger, Security $security)
     {
+        if (!$category) {
+            throw new NotFoundHttpException("Cette catégory n'existe pas");
+        }
+
+        // $this->denyAccessUnlessGranted('CAN_EDIT', $category, "Vous n'êtes pas le propriétaire de cette catégorie, par conséquent vous ne pouvez pas la modifier");
+
+        // $user = $this->getUser();
+
+        // if (!$user) {
+        //     return $this->redirectToRoute("security_login");
+        // }
+
+        // if ($user !== $category->getOwner()) {
+        //     throw new AccessDeniedHttpException("Vous n'êtes pas le propriétaire de cette catégorie, par conséquent vous ne pouvez pas la modifier");
+        // }
+
+        // $this->denyAccessUnlessGranted("ROLE_ADMIN", null, "Vous n'avez pas le droit d'accéder à cette ressource");
+
+        // $user = $security->getUser();
+        // $user = $this->getUser();
+
+        // if ($user === null) {
+        //     return $this->redirectToRoute('security_login');
+        // }
+
+        // if (!in_array('ROLE_ADMIN', $user->getRoles())) {
+        //     throw new AccessDeniedHttpException("Vous n'avez pas le droit d'accéder à cette ressource");
+        // }
+
+        // if ($security->isGranted("ROLE_ADMIN") === false) {
+        //     throw new AccessDeniedHttpException("Vous n'avez pas le droit d'accéder à cette ressource");
+        // }
+
+        // if ($this->isGranted("ROLE_ADMIN") === false) {
+        //     throw new AccessDeniedHttpException("Vous n'avez pas le droit d'accéder à cette ressource");
+        // }
+
+
         $form = $this->createForm(CategoryType::class, $category);
         $form->handleRequest($request);
 
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,20 +2,24 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Category;
 use Faker\Factory;
+use App\Entity\User;
 use App\Entity\Product;
+use App\Entity\Category;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
     protected $slugger;
+    protected $encoder;
 
-    public function __construct(SluggerInterface $slugger)
+    public function __construct(SluggerInterface $slugger, UserPasswordEncoderInterface $encoder)
     {
         $this->slugger = $slugger;
+        $this->encoder = $encoder;
     }
 
     public function load(ObjectManager $manager)
@@ -24,6 +28,25 @@ class AppFixtures extends Fixture
         $faker->addProvider(new \Liior\Faker\Prices($faker));
         $faker->addProvider(new \Bezhanov\Faker\Provider\Commerce($faker));
         $faker->addProvider(new \Bluemmb\Faker\PicsumPhotosProvider($faker));
+
+        $admin = new User();
+        $password = $this->encoder->encodePassword($admin, 'password');
+        $admin->setEmail("admin@gmail.com")
+            ->setFullName("Admin")
+            ->setPassword($password)
+            ->setRoles(['ROLE_ADMIN']);
+
+        $manager->persist($admin);
+
+        for ($u = 0; $u < 5; $u++) {
+            $user = new User();
+            $password = $this->encoder->encodePassword($user, 'password');
+            $user->setEmail("user$u@gmail.com")
+                ->setFullName($faker->name())
+                ->setPassword($password);
+
+            $manager->persist($user);
+        }
 
         for ($c = 0; $c < 3; $c++) {
             $category = new Category();
